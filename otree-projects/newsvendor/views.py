76280897365c030a9d85ee90294f94606a2d2c23
miller-moore <gmoore316@gmail.com>
2021-09-10 12:55:33 -0400

    add distribution data & load from static csv file

--- otree-projects/newsvendor/views.py
+++ otree-projects/newsvendor/views.py
@@ -1,4 +1,5 @@
 from decimal import ROUND_HALF_UP, Decimal
+from pathlib import Path
 
 from otree.api import Currency as c
 from otree.api import currency_range, safe_json
@@ -6,6 +7,19 @@ from otree.api import currency_range, safe_json
 from . import models
 from ._builtin import Bot, Page, WaitPage
 from .models import Constants, profit, set_time, trueorderquantity
+
+# demand_distributions_csv = Path(__file__).resolve().parent / "templates" / "newsvendor" / "demand_distributions.csv"
+app_directory = Path(__file__).resolve().parent
+demand_distributions_csv = app_directory / "static" / "demand_distributions.csv"
+if not demand_distributions_csv.exists():
+    import numpy as np
+    import pandas as pd
+
+    mu, sigma1, sigma2 = 500, 50, 100
+    dist1 = np.random.normal(loc=mu, scale=sigma1, size=(int(1e6),))
+    dist2 = np.random.normal(loc=mu, scale=sigma2, size=(int(1e6),))
+    df = pd.DataFrame({"d1": dist1, "d2": dist2})
+    df.to_csv(demand_distributions_csv, header=True, index=False)
 
 
 class FirstWelcomePage(Page):
@@ -16,6 +30,14 @@ class FirstWelcomePage(Page):
     @staticmethod
     def is_displayed(player):
         return player.round_number == 1
+
+    @staticmethod
+    def vars_for_template(player):
+        return dict(
+            demand_distributions_csv=str(
+                Path.joinpath(Path("/"), Path(demand_distributions_csv.parent.name), Path(demand_distributions_csv.name))
+            )
+        )
 
 
 class WelcomePage(Page):
